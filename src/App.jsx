275894143b368fd5b70d115a5c1462813ba81538
Jamshed Alam <jamshedalam1777@gmail.com>
2025-06-28 15:35:57 +0530

    redux toolkit setup for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Head from "./components/Head"
 import Body from "./components/Body"
+import store from "./utills/store";
 function App() {
   return (
+    <provider store={store}>
     <div>
       <Head />
       <Body />
      
 
     </div>
+    </provider>
 
    
-  )
+  );
     
     
   
