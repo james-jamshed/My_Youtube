bd6a6f78ca3a3ff0f92a529e185f68474395c7ba
Jamshed Alam <jamshedalam.1777@gmail.com>
2025-10-15 11:30:09 +0530

    added video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,7 +6,7 @@ const VideoCard = ({ info }) => {
     const { channelTitle, title, thumbnails } = snippet;
     
   return (
-      <div className="p-2 m-2 w-56">
+      <div className="p-2 m-2 w-72">
           <img alt='thumnail' src={thumbnails?.medium?.url} />
           <ul>
               <li>{title}</li>
